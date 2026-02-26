4e51033512160ade62a3efb96f6cc7c801604be2
Ayla81 <354231362@educ.dpcdsb.org>
2026-02-26 12:59:15 -0800

    Variables Make

---
+++ vars_make.py
@@ -0,0 +1,19 @@
+
+"""
+Author: Ayla Laone
+Date: 02-26-2026
+Variables Make
+"""
+
+#Input
+r_circle = input("The radius of the circle:")
+l_rect = input("The length of the rectangle:")
+w_rect = input("The width of the rectangle:")
+l_oct = input("A side length of the octagon:")
+
+
+
+#Processing
+
+
+#Output    
