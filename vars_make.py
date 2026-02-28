f6a9f3e397befd5b5b74c90ae3809447fe6b964b
Ayla81 <354231362@educ.dpcdsb.org>
2026-02-27 19:27:11 -0500

    vars_make complete

--- vars_make.py
+++ vars_make.py
@@ -1,19 +1,36 @@
 
 """
-Author: Ayla Laone
-Date: 02-26-2026
-Variables Make
+# Author: Ayla Laone
+# Date: 02-27-2026
+# Description: Variables Make
 """
 
 #Input
-r_circle = input("The radius of the circle:")
-l_rect = input("The length of the rectangle:")
-w_rect = input("The width of the rectangle:")
-l_oct = input("A side length of the octagon:")
 
+circle_radius = 'int(input("The radius of the circle:"))'
+rect_length = 'int(input("The length of the rectangle:"))'
+rect_width = 'int(input("The width of the rectangle:"))'
+octa_side = 'int(input("A side length of the octagon:"))'
 
 
 #Processing
 
+import math
 
-#Output    
+circle_area = math.pi * circle_radius ** 2
+circle_perimeter = 2 * math.pi * circle_radius
+
+
+rect_area = rect_length * rect_width
+rect_perimeter = (rect_length + rect_width) * 2
+
+
+octa_area = 2 * (1 + math.sqrt(2)) * octa_side ** 2
+octa_perimeter = octa_side * 8
+
+
+#Output
+
+print(f"The circle has an area of {circle_area} and a perimeter of {circle_perimeter}")
+print(f"The rectangle has an area of {rect_area} and a perimeter of {rect_perimeter}")
+print(f"The octagon has an area of {octa_area} and a perimeter of {octa_perimeter}")
